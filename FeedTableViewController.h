b79375c0288adc72be9feb8f1b10003a7151a764
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-17 14:10:52 -0400

    added FeedTableViewController

---
+++ FeedTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FeedTableViewController.h
+//  BMDay10Movies
+//
+//  Created by Kunwardeep Gill on 2015-04-17.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedTableViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
+
+
+
+@end
